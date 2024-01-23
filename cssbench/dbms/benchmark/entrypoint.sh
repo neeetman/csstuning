d46843323a5c8717c6393bf37fc3a16f7c2b9c31
neeetman <anshaohac@gmail.com>
2024-01-23 06:17:49 +0000

    update for debug

--- cssbench/dbms/benchmark/entrypoint.sh
+++ cssbench/dbms/benchmark/entrypoint.sh
@@ -7,5 +7,9 @@ set -eu
 cd /benchbase
 if ! [ -d results/ ] || ! [ -w results/ ]; then
     echo "ERROR: The results directory either doesn't exist or isn't writable." >&2
+    exit 1
 fi
 exec java -jar benchbase.jar $*
+
+echo "Listing contents of the results directory:"
+ls -l results/
